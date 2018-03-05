44966e0fad73ca71dc0b89f67bd71098600be8af
Ron Craig <ron.craig@comcast.net>
2018-03-05 01:38:53 -0800

    edit erb files and app.rb file for front end planning

--- app.rb
+++ app.rb
@@ -6,3 +6,25 @@ require('./lib/store')
 require('./lib/shoe')
 require('pg')
 require('pry')
+
+
+get('/') do
+  @stores = Store.all()
+  @shoes = Shoe.all()
+  erb(:index)
+end
+
+get('/stores') do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+post("/store/edit") do
+  name = params[:name]
+  @store = Store.new({:name => name, :done => false})
+  if @store.save()
+    erb(:success)
+  else
+    erb(:store_errors)
+  end
+end
