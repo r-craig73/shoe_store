4516ec5f0dbcd6b78f3bf0f29c4acfaa1968a82c
Ron Craig <ron.craig@comcast.net>
2018-03-05 00:52:21 -0800

    fail and pass store name presence

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,6 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:shoes)
+  validates(:name, :presence => true)
   before_save(:titleize_name)
 
   def titleize_name
--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -4,7 +4,13 @@ describe(Store) do
   it {should have_and_belong_to_many(:shoes)}
 
   it("converts the first letter of each word to be an uppercase letter") do
-    store = Store.create({:name => "wildwood"})
-    expect(store.titleize_name()).to(eq("Wildwood"))
+    store = Store.create({:name => "east wildwood"})
+    expect(store.titleize_name()).to(eq("East Wildwood"))
   end
+
+  it("validates presence of a store name") do
+    store = Store.new({:name => ""})
+    expect(store.save()).to(eq(false))
+  end
+
 end
