d249367a739965d11322a401bedc480587a9f7e9
Ron Craig <ron.craig@comcast.net>
2018-03-05 01:08:56 -0800

    pass store and brand names exceeding 100 characters, 2 specs (8 out of 8)

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,7 @@
 class Shoe < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   validates(:brand, :presence => true)
+  validates(:brand, {:presence => true, :length => { :maximum => 100 }})
   before_save(:titleize_name)
 
   def titleize_name
--- lib/store.rb
+++ lib/store.rb
@@ -1,6 +1,7 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:shoes)
   validates(:name, :presence => true)
+  validates(:name, {:presence => true, :length => { :maximum => 100 }})
   before_save(:titleize_name)
 
   def titleize_name
